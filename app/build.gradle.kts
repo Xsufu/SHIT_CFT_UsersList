c51afc0453fa8994fe2cca439e7e49b8e33773e6
Xsufu <aluno.macarrao@gmail.com>
2024-01-28 00:03:06 +0700

    Подключена библиотека Picasso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,12 @@ dependencies {
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
+    // Picasso
+    implementation("com.squareup.picasso:picasso:2.8")
+
+    // SwipeRefreshLayout
+    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
