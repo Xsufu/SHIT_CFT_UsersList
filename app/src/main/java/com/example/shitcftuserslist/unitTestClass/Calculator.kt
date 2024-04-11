f9ed2c7b5a06c52c9449b50f81161f2b3cc27a6f
Xsufu <aluno.macarrao@gmail.com>
2024-04-11 14:36:22 +0700

    Создан класс калькулятора с функциями возведения в степень, сложения и вычитания

---
+++ app/src/main/java/com/example/shitcftuserslist/unitTestClass/Calculator.kt
@@ -0,0 +1,12 @@
+package com.example.shitcftuserslist.unitTestClass
+
+import java.lang.Math.pow
+import kotlin.math.pow
+
+class Calculator {
+    fun exponentiation(number: Double, power: Double): Double = number.pow(power)
+
+    fun addition(firstSummand: Int, sndSummand: Int): Int = firstSummand + sndSummand
+
+    fun difference (minuend: Int, subtrahend: Int): Int = minuend - subtrahend
+}
