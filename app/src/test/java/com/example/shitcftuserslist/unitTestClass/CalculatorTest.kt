0b02e54fafbbee22cf8a85562c0332d19dbadf30
Xsufu <aluno.macarrao@gmail.com>
2024-04-11 15:01:18 +0700

    Добавил параметризованный тест

--- app/src/test/java/com/example/shitcftuserslist/unitTestClass/CalculatorTest.kt
+++ app/src/test/java/com/example/shitcftuserslist/unitTestClass/CalculatorTest.kt
@@ -3,9 +3,24 @@ package com.example.shitcftuserslist.unitTestClass
 import org.junit.jupiter.api.Assertions.*
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class CalculatorTest {
     private val calculator = Calculator()
+
+    private companion object {
+
+        @JvmStatic
+        fun data(): Stream<Arguments> = Stream.of(
+            arguments(3.0, 2.0, 9.0),
+            arguments(5.0, 0.0, 1.0),
+            arguments(4.0, -2.0, 0.0625)
+        )
+    }
 
     @Test
     fun `WHEN 2 is raised to power 5 EXPECT 32`() {
@@ -30,6 +45,14 @@ class CalculatorTest {
         val expected: Int = 15
 
         val actual = calculator.difference(minuend = 32, subtrahend = 17)
+
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    fun `WHEN raise to power EXPECT correct result`(number: Double, power: Double, expected: Double) {
+        val actual = calculator.exponentiation(number, power)
 
         assertEquals(expected, actual)
     }
